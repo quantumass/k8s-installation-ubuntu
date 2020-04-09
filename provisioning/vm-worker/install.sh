962c65785082ed9fe2425c27e1c1ede4742c66a0
soufiane masmoud <masmoud@live.com>
2020-04-09 11:44:56 +0100

    Update install.sh

--- provisioning/vm-worker/install.sh
+++ provisioning/vm-worker/install.sh
@@ -2,9 +2,6 @@
 
 figlet WORKER
 
-figlet NFS
-apt-get install nfs-common
-
 echo "[TASK 1] Join master"
 #Update the join token by executing the command on master node
 #kubeadm token create --print-join-command
